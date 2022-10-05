54ce641967e5945ed413ef3ea40cb41889b87600
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-10-05 10:27:54 -0700

    STUBBING COMPLETED

---
+++ index.js
@@ -0,0 +1,37 @@
+//REQUIRE LIBRARIES
+const inquirer = require("inquirer");
+const fs =require("fs");
+
+//GLOBAL VARIABLES
+
+
+// QUESTIONS THAT ARE REQUIRED BY USER STORY HERE
+
+//MAIN MENU QUESTIONS
+// view all departments,
+// view all roles, 
+// view all employees, 
+// add a department, 
+// add a role, 
+// add an employee, 
+// update an employee role
+
+//QUESTIONS FOR ADDING A DEPT
+
+//QUESTIONS TO ADD A ROLE
+
+//QUESTIONS TO ADD AN EMPLOYEE
+
+//QUESTIONS TO UPDATE AN EMPLOYEE
+
+// FUNCTION TO LINK THE QUESTIONS
+
+//INITIALIZING FUNCTION
+    //USE SWITCH SYNTAX TO SELECT OPTIONS
+
+//FUNCTIONS TO SHOW EACH OPTIONS (SO MANY)
+
+//FUNCTIONS TO ADD EACH OPTIONS
+
+//ACTION 👏 CODE 👏
+init()
