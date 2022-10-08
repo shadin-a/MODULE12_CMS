ad7f732eb199418ffc925536650626470206f7de
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-10-07 19:30:41 -0700

    linkage from index to server w/o db

--- index.js
+++ index.js
@@ -1,43 +1,116 @@
 //REQUIRE LIBRARIES
-const inquirer = require("inquirer");
-const fs =require("fs");
-
+import inquirer from 'inquirer';
+import fs from "fs";
+import {MyDatabaseInterface} from './server.js';
+const db = new MyDatabaseInterface();
 //GLOBAL VARIABLES
+var roles = [];
 
-
-// QUESTIONS THAT ARE REQUIRED BY USER STORY HERE
-dbInterface = new MyDatabaseInterface();
-
+//// QUESTIONS THAT ARE REQUIRED BY USER STORY HERE
 //MAIN MENU QUESTIONS
-// view all departments,
-// view all roles, 
-// view all employees, 
-// add a department, 
-// add a role, 
-// add an employee, 
-// update an employee role
+const mainMenuQuestions =[
+    {
+        type: "list",
+        message: "What would you like to do today?",
+        name: "menu",
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
+    },
+  
+]
 
 //QUESTIONS FOR ADDING A DEPT
+const departmentQuestions = [
+    {
+        type: "input",
+        message: "What is the name of your new department?",
+        name: "department_name",
+        default: "Deperatment Name Missing"
+    },
 
+]
 //QUESTIONS TO ADD A ROLE
-
+const roleQuestions =[
+    {
+        type: "input",
+        message:"What is the name of the new role?",
+        name: "role_name",
+        default: "Missing role name",
+    },
+    {
+        type: "input",
+        message:"What is the salary for the new role?",
+        name: "role_salary",
+        default: "Missing salary",
+    },
+    {
+        type: "list",
+        message:"What department does this new role belong to?",
+        name: "role_department",
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+    },
+]
 //QUESTIONS TO ADD AN EMPLOYEE
+const employeeQuestions = [
+    {
+        type: "input",
+        message:"What is the first name of the new employee?",
+        name: "employee_first_name",
+        default: "Missing employee first name",
+    },
+    {
+        type: "input",
+        message:"What is the last name of the new employee?",
+        name: "employee_last_name",
+        default: "Missing employee last name",
+    },
+    {
+        type: "list",
+        message:"What is the role of the new employee?",
+        name: "employee_first_name",
+        choices: [roles], // TO DO: create variable to hold roles...database
+    },
+    {
+        type: "input",
+        message:"Who is the manager of the new employee?",
+        name: "manager_first_name",
+        default: "Missing manager name",
+    },
 
-if (they want to fetch all emplyoees) {
-    allEmployees = dbInterface.getAllEmployees();
-    // maybe you wanna format htat
-    console.log(allEmployees);
-
-}
+]
 
 //QUESTIONS TO UPDATE AN EMPLOYEE
+const employeeUpdateQuestions = [
+    {
+        type: "list",
+        name: "new_role",
+        choices: [roles],
+    }
+]
 
-// FUNCTION TO LINK THE QUESTIONS
-
+//// FUNCTION TO LINK THE QUESTIONS
 //INITIALIZING FUNCTION
+function init() {
+    
+    inquirer.prompt(mainMenuQuestions)
+        .then(answer => {
+            console.table(answer);
+        
     //USE SWITCH SYNTAX TO SELECT OPTIONS
+    switch(answer.menu){
+        case "View all departments":
+            var departments = db.getAllDepartments();
+            console.log(departments);
 
+        return
+    }
+
+})
+};
 //FUNCTIONS TO SHOW EACH OPTIONS (SO MANY)
+// if (they want to fetch all emplyoees) {
+//     allEmployees = dbInterface.getAllEmployees();
+//     // maybe you wanna format htat
+//     console.log(allEmployees);
 
 //FUNCTIONS TO ADD EACH OPTIONS
 
--- server.js
+++ server.js
@@ -5,33 +5,37 @@
 
 
 
-class MyDatabaseInterface {
+export class MyDatabaseInterface {
 
-        // private funciton, this only gets called WITHIN this class
-    function getConnection() {
-        return mysql.createConnection({
-          host: "localhost",
-          user: "yourusername",
-          password: "yourpassword",
-          database: "mydb"
-        });
+    //     // private funciton, this only gets called WITHIN this class
+    // function getConnection() {
+    //     return mysql.createConnection({
+    //       host: "localhost",
+    //       user: "yourusername",
+    //       password: "yourpassword",
+    //       database: "mydb"
+    //     });
+    // }
+
+    //     // public function -> clients of this class can call this function
+    // function getAllEmployees() {
+    //     con = self.getConnection()
+    //     con.connect();
+    //     result = con.query('SELECT * FROM MYDATABASE WHERE ROLE == EMPLYOEE');
+    //     console.log(result);
+    //     return result;
+    // }
+
+    getAllDepartments() {
+        return "hello world";
     }
 
-        // public function -> clients of this class can call this function
-    function getAllEmployees() {
-        con = self.getConnection()
-        con.connect();
-        result = con.query('SELECT * FROM MYDATABASE WHERE ROLE == EMPLYOEE');
-        console.log(result);
-        return result;
-    }
-
-    function insertEmployee(name, role, etc....){
-    con = self.getConnection()
-    con.connect();
-    result = con.query(insert into my table (bla the data i forget the syntax));
-    }
-}
+//     function insertEmployee(name, role, etc....){
+//     con = self.getConnection()
+//     con.connect();
+//     result = con.query(insert into my table (bla the data i forget the syntax));
+//     }
+// }
 
     // test 1 -> empty database, run this, it should call it w/o error and return an empty array
     // test 2 -> put in employees, run this, it should return an arra of employee data
